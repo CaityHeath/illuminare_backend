cd5d6d7e936a4c02d9cbd87a0ecf4b0bf62f91d9
CaityHeath <caity.heath@gmail.com>
2020-01-13 15:35:03 -0800

    fetching data from the google civic api using a client request

---
+++ server.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const express = require('express');
+const superagent = require('superagent');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT
+const key = process.env.google_civic
+app.use(cors());
+
+app.get('/', home);
+app.get('/elections', getElections);
+
+
+function home(req, res){
+  console.log('home route');
+  return res.send('Hello World');
+}
+
+function getElections(req, res){
+  console.log('route hit');
+  let query = req.query.query;
+
+  let url = `https://www.googleapis.com/civicinfo/v2/representatives?key=${key}&address=${query}`;
+
+  return superagent.get(url)
+    .then(response => {
+      console.log(response.body);
+      return res.json(response.body);
+    }).catch(err => handleError(err, res));
+}
+
+
+function handleError(err, res){
+  console.log(err);
+  if(res) res.status(500).send('Sorry something went wrong');
+}
+
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+
+// https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyAAoE7GA9s53_NrusNCv0RVW_h2hZxW-FY?address=4241%20Greenwood%20Ave.%20N%20Seattle%20Wa
+// https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyAAoE7GA9s53_NrusNCv0RVW_h2hZxW-FY&address=4241%20Greenwood%20Ave.%20N%20Seattle%20Wa
